1286d4a5c51c21629ce1570c37ab2b29d32c1f3e
lvcyklm02 <lvcyklm02@gmail.com>
2022-01-11 16:57:43 -0600

    Auth ClientId
    Replaced placeholder clientId with project's unique google auth clientId.

--- client/src/components/pages/Skeleton.js
+++ client/src/components/pages/Skeleton.js
@@ -5,7 +5,7 @@ import "../../utilities.css";
 import "./Skeleton.css";
 
 //TODO: REPLACE WITH YOUR OWN CLIENT_ID
-const GOOGLE_CLIENT_ID = "121479668229-t5j82jrbi9oejh7c8avada226s75bopn.apps.googleusercontent.com";
+const GOOGLE_CLIENT_ID = "906485839568-enlavp0b4pbshg8vaopc8019aet1rfie.apps.googleusercontent.com";
 
 const Skeleton = ({ userId, handleLogin, handleLogout }) => {
   return (
