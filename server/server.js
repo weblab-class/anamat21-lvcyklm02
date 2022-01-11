8c21e95ddb2e93377b81b6333c9c4c60ee9e08a0
anamat21 <anacamillemata21@gmail.com>
2022-01-11 17:50:43 -0500

    Connected to MongoDB

--- server/server.js
+++ server/server.js
@@ -33,9 +33,10 @@ const socketManager = require("./server-socket");
 
 // Server configuration below
 // TODO change connection URL after setting up your team database
-const mongoConnectionURL = "FILL ME IN";
+const mongoConnectionURL =
+  "mongodb+srv://ana_and_lucy:kzaIy7VaWxl3OAiL@cluster0.vnagq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 // TODO change database name to the name you chose
-const databaseName = "FILL ME IN";
+const databaseName = "podmates";
 
 // connect to mongodb
 mongoose
