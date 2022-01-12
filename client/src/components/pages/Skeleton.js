ae9606c3e5d4326a651b541975bc96fa50e27caf
anamat21 <anacamillemata21@gmail.com>
2022-01-12 17:57:11 -0500

    Update Skeleton.js

--- client/src/components/pages/Skeleton.js
+++ client/src/components/pages/Skeleton.js
@@ -25,22 +25,14 @@ const Skeleton = ({ userId, handleLogin, handleLogout }) => {
           onFailure={(err) => console.log(err)}
         />
       )}
-      <h1>Good luck on your project :)</h1>
-      <h2> What you need to change in this skeleton</h2>
-      <ul>
-        <li>
-          Change the Frontend CLIENT_ID (Skeleton.js) to your team's CLIENT_ID (obtain this at
-          http://weblab.to/clientid)
-        </li>
-        <li>Change the Server CLIENT_ID to the same CLIENT_ID (auth.js)</li>
-        <li>
-          Change the Database SRV (mongoConnectionURL) for Atlas (server.js). You got this in the
-          MongoDB setup.
-        </li>
-        <li>Change the Database Name for MongoDB to whatever you put in the SRV (server.js)</li>
-      </ul>
-      <h2>How to go from this skeleton to our actual app</h2>
-      <a href="http://weblab.to/get-started">Check out this getting started guide</a>
+      <div>
+        <h1>Welcome to PodMates!</h1>
+        <h3>Little introduction</h3>
+      </div>
+      <div>
+        <h1>Make a new PodMates living community:</h1>
+        <h1>Join an existing living community:</h1>
+      </div>
     </>
   );
 };
